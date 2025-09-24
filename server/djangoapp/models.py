1831e7be185877fcc8e72753310dbdf58dd33a4f
Devang Bhuvad <devangbhuvad2004@gmail.com>
2025-09-24 13:42:12 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,27 +1,30 @@
 from django.db import models
-#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-# Car Model model
-class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
-    name = models.CharField(max_length=100)
 
+class CarModel(models.Model):
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default='SUV'
+    )
     year = models.IntegerField(
         default=2023,
         validators=[
@@ -29,8 +32,6 @@ class CarModel(models.Model):
             MinValueValidator(2015)
         ]
     )
-
-    # You can add other fields like price, color, etc.
 
     def __str__(self):
         return f"{self.car_make.name} {self.name} ({self.year})"
