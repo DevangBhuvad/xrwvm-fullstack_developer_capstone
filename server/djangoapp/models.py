1db2d9398717523e54445ceb94b5fe0e5b87b8e6
Devang Bhuvad <devangbhuvad2004@gmail.com>
2025-09-24 13:36:24 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,16 +1,13 @@
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Car Make model
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # You can add more fields if needed (e.g., country, founded_year)
-
+    
     def __str__(self):
         return self.name  # Return the name as the string representation
-
 
 # Car Model model
 class CarModel(models.Model):
